631298e44ac1029bcff06b380f327e94e72f1f53
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2024-04-17 22:54:16 +0530

    update 1

--- src/tools/commands.tsx
+++ src/tools/commands.tsx
@@ -31,7 +31,7 @@ export const consoleOuput = (
             return (
                 <div className="w-3/4">
                     Hi! Thanks for social researching(I don't like the other s word) on me!<br /> I am{" "} <span className="text-purple-400">Ankit Patnaik</span>, a 3rd year undergraduate student at{" "} <span className="text-teal-400">{" "}
-                    <a href="http://nita.ac.in">National Institute of Technology Agartala</a></span> pursuing B.Tech in Electrical Engineering. I have predominantly focused on web technologies for my work, primarily delving into MERN stack. However, I've recently started exploring core computer science disciplines to deepen my understanding. Alongside my proficiency in MERN, I am also adept at utilizing Python and Go for various projects and tasks.<br /><br />
+                    <a href="http://nita.ac.in" target="_blank">National Institute of Technology Agartala</a></span> pursuing B.Tech in Electrical Engineering. I have predominantly focused on web technologies for my work, primarily focusing on MERN. However, I've recently started exploring core computer science disciplines to deepen my understanding. Alongside my proficiency in MERN, I am also skilled at utilizing Python and Go for various projects and tasks.<br /><br />
 
                     <span className="text-blue-400">PS:</span> Just as <span className="text-teal-400">Gollum</span> aided <span className="text-teal-400">Frodo</span> and <span className="text-teal-400">Sam</span> in navigating their way through the perilous realm of Mordor, here he stands prepared to guide you through my portfolio!
                 </div>
@@ -43,22 +43,22 @@ export const consoleOuput = (
                         <span className="text-teal-400">
                             [Mail]: {" "}
                         </span>
-                        <a href="mailto:ankit.patnaik2003@gmail.com" className="underline">ankit.patnaik2003@gmail.com</a><br />
+                        <a href="mailto:ankit.patnaik2003@gmail.com">ankit.patnaik2003@gmail.com</a><br />
 
                         <span className="text-teal-400">
                             [LinkedIn]: {" "}
                         </span>
-                        <a href="https://www.linkedin.com/in/ankit-patnaik-102728230/" className="underline">https://www.linkedin.com/in/ankit-patnaik-102728230</a>{" "}[DM's preferred here]<br />
+                        <a href="https://www.linkedin.com/in/ankit-patnaik-102728230/" target="_blank">ankit-patnaik-102728230</a>{" "}[DM's preferred here]<br />
 
                         <span className="text-teal-400">
                             [Github]: {" "}
                         </span>
-                        <a href="https://github.com/patnaikankit" className="underline">https://github.com/patnaikankit</a><br />
+                        <a href="https://github.com/patnaikankit" target="_blank">patnaikankit</a><br />
 
                         <span className="text-teal-400">
                             [Twitter]: {" "}
                         </span>
-                        <a href="https://twitter.com/_Ankitpatnaik1_" className="underline">https://twitter.com/_Ankitpatnaik1_</a><br />
+                        <a href="https://twitter.com/_Ankitpatnaik1_" target="_blank">@_Ankitpatnaik1_</a><br />
                     </div>
                 );
 
@@ -91,7 +91,7 @@ export const consoleOuput = (
                       <div className="w-3/4">
                       -{" "}A platform that enables users to create futuristic images based on the prompt provided by the user.<br />
                       </div>
-                      -{"  "}Link: <a href="https://github.com/patnaikankit/Imagix" className="text-blue-500">[Github]</a>{" | "}<a href="https://imagix-web.netlify.app/" className="text-blue-500">[Live]</a><br />
+                      -{"  "}Link: <a href="https://github.com/patnaikankit/Imagix" className="text-blue-500" target="_blank">[Github]</a>{" | "}<a href="https://imagix-web.netlify.app/" className="text-blue-500" target="_blank">[Live]</a><br />
                       -{"  "}Technologies: Tailwind, DaisyUI, Typescript, Express.js, MongoDB, OpenAI API<br />
 
                     -----------------------------------------------------------------------------------------------------------------------------<br /><br />
@@ -103,7 +103,7 @@ export const consoleOuput = (
                       <div className="w-3/4">
                       -{"  "}A robust platform to facilitate secure file sharing and chat between two users without the brokerage of a third party.<br />
                       </div>
-                      -{"  "}Link: <a href="https://github.com/patnaikankit/LiteShare" className="text-blue-500">[Github]</a>{" | "}<a href="https://lite-share.vercel.app/" className="text-blue-500">[Live]</a><br />
+                      -{"  "}Link: <a href="https://github.com/patnaikankit/LiteShare" className="text-blue-500" target="_blank">[Github]</a>{" | "}<a href="https://lite-share.vercel.app/" className="text-blue-500" target="_blank">[Live]</a><br />
                       -{"  "}Technologies: React, Typescript, Node.js, Next.js, Socket.io, WebRTC<br />
 
                       -----------------------------------------------------------------------------------------------------------------------------<br /><br />
@@ -113,8 +113,28 @@ export const consoleOuput = (
                         <div className="text-teal-400 inline"> Youtube Summarizer</div> <br />
                     </div>
                       -{"  "}An AI tool that can generate a summary based on the YouTube video provided to it.<br />
-                      -{"  "}Link: <a href="https://github.com/patnaikankit/Youtube-Summarizer" className="text-blue-500">[Github]</a>{" | "}<a href="https://youtube-summarizer-seven.vercel.app/" className="text-blue-500">[Live]</a><br />
+                      -{"  "}Link: <a href="https://github.com/patnaikankit/Youtube-Summarizer" className="text-blue-500" target="_blank">[Github]</a>{" | "}<a href="https://youtube-summarizer-seven.vercel.app/" className="text-blue-500" target="_blank">[Live]</a><br />
                       -{"  "}Technologies: Tailwind, Javascript, Django, Postgresql, Assembly AI API, OpenAI API<br />
+
+                      -----------------------------------------------------------------------------------------------------------------------------<br /><br />
+
+                    <div className="flex items-center">
+                        <span className="text-purple-400 mr-1">4. </span>
+                        <div className="text-teal-400 inline"> POS-API</div> <br />
+                    </div>
+                      -{"  "}A Point of Sale API for everyday retail transactions.<br />
+                      -{"  "}Link: <a href="https://github.com/patnaikankit/POS-API" className="text-blue-500" target="_blank">[Github]</a><br />
+                      -{"  "}Technologies: Go, Fiber, MySQL<br />  
+
+                      -----------------------------------------------------------------------------------------------------------------------------<br /><br />
+
+                    <div className="flex items-center">
+                        <span className="text-purple-400 mr-1">5. </span>
+                        <div className="text-teal-400 inline"> CF-Diary</div> <br />
+                    </div>
+                      -{"  "}Stalk codeforces profiles in a cool way.<br />
+                      -{"  "}Link: <a href="https://github.com/patnaikankit/cf-diary" className="text-blue-500" target="_blank">[Github]</a>{" | "}<a href="https://cf-diary.netlify.app/" className="text-blue-500" target="_blank">[Live]</a><br />
+                      -{"  "}Technologies: React, Tailwind, Google-Charts-API, Codeforces-API<br />  
 
                 </div>
             );
