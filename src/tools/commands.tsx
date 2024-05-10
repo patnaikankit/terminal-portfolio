a3ae45b06b0e06b25d86e12be06b9674e877fa65
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2024-05-10 11:38:00 +0530

    resume chnage

--- src/tools/commands.tsx
+++ src/tools/commands.tsx
@@ -165,7 +165,7 @@ export const consoleOuput = (
                     </div>
                 )
 
-            case "resume": window.open("https://drive.google.com/file/d/1EQA1TH34e2NJb-nufvEFjgJyvuI7-ILD/view?usp=sharing", "_blank");
+            case "resume": window.open("https://drive.google.com/file/d/1Vkj5GzBlUI4SfwIyh2WLoG1wIJP_C_vS/view?usp=sharing", "_blank");
             return "Opening resume in new tab......"
 
             case "clear": 
